9e827ed1c85b350447bc3e5ed108eb32b860fd20
jumerckx <31353884+jumerckx@users.noreply.github.com>
2026-01-01 18:00:43 +0100

    saturate

--- src/TamatchDialect.cpp
+++ src/TamatchDialect.cpp
@@ -33,6 +33,7 @@
 #include "llvm/Support/Debug.h"
 #include "llvm/Support/ErrorHandling.h"
 #include <cassert>
+#include <chrono>
 #include <utility>
 
 #define DEBUG_TYPE "tamatch"
@@ -110,12 +111,19 @@ struct TamatchSaturatePass
       llvm::cl::desc("Whether to throw an error when an operation is removed "
                      "during equality saturation, defaults to true."),
       llvm::cl::init(true)};
+  Option<int> maxIters{
+      *this, "max-iters",
+      llvm::cl::desc("Maximum number of iterations before equality saturation "
+                     "times out."),
+      llvm::cl::init(4)};
 
   void getDependentDialects(DialectRegistry &registry) const override {
     registry.insert<mlir::pdl_interp::PDLInterpDialect>();
   }
 
   void runOnOperation() final {
+    auto startTime = std::chrono::high_resolution_clock::now();
+
     ModuleOp module = getOperation();
 
     // Load pattern and IR modules from input
@@ -136,7 +144,7 @@ struct TamatchSaturatePass
     EqOpUnionFind uf{};
     HashConsPatternRewriter rewriter(module.getContext());
     ScopedMapTy &hashcons = rewriter.hashcons;
-    ScopedMapTy::ScopeTy scope(hashcons);
+    ScopedMapTy::ScopeTy rootScope(hashcons);
 
     irModule.walk([&](Operation *op) {
       if (dyn_cast<tama::EqOp>(*op)) {
@@ -212,31 +220,50 @@ struct TamatchSaturatePass
     // This manages memory for matches. Crucially, we keep this alive
     // between the Match phase and the Rewrite phase.
     mlir::detail::PDLByteCodeMutableState bytecodeState;
-    bytecode->initializeMutableState(bytecodeState);
 
-    // Walk the IR and collect ALL matches for ALL operations.
-    irModule.walk([&](Operation *op) {
-      SmallVector<mlir::detail::PDLByteCode::MatchResult, 4> opMatches;
+    int nIters = 0;
 
-      // Execute the bytecode matcher.
-      // matches are allocated in bytecodeState and pointers are stored in
-      // opMatches.
-      bytecode->match(op, rewriter, opMatches, bytecodeState);
-
-      for (auto &match : opMatches) {
-        allMatches.push_back({op, std::move(match)});
+    do {
+      nIters++;
+      LLVM_DEBUG(llvm::dbgs() << "Equality saturation: starting iteration "
+                              << nIters << "\n");
+      if (nIters > maxIters) {
+        break;
       }
-    });
 
-    // Apply rewrites for all collected matches.
-    for (const auto &pm : allMatches) {
-      // Set insertion point to the matched operation (standard PDL behavior)
-      rewriter.setInsertionPoint(pm.op);
+      bytecode->initializeMutableState(bytecodeState);
 
-      // Execute the rewrite. This will trigger the registered "union" callback.
-      // We pass the same bytecodeState so it can access captured values.
-      (void)bytecode->rewrite(rewriter, pm.matchResult, bytecodeState);
-    }
+      // Walk the IR and collect ALL matches for ALL operations.
+      irModule.walk([&](Operation *op) {
+        SmallVector<mlir::detail::PDLByteCode::MatchResult, 4> opMatches;
+
+        // Execute the bytecode matcher.
+        // matches are allocated in bytecodeState and pointers are stored in
+        // opMatches.
+        bytecode->match(op, rewriter, opMatches, bytecodeState);
+
+        for (auto &match : opMatches) {
+          allMatches.push_back({op, std::move(match)});
+        }
+      });
+
+      // Apply rewrites for all collected matches.
+      for (const auto &pm : allMatches) {
+        // Set insertion point to the matched operation (standard PDL behavior)
+        rewriter.setInsertionPoint(pm.op);
+
+        // Execute the rewrite. This will trigger the registered "union"
+        // callback. We pass the same bytecodeState so it can access captured
+        // values.
+        (void)bytecode->rewrite(rewriter, pm.matchResult, bytecodeState);
+      }
+      bytecodeState.cleanupAfterMatchAndRewrite();
+    } while (uf.rebuild(rewriter));
+    auto endTime = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(
+        endTime - startTime);
+    LLVM_DEBUG(llvm::dbgs()
+               << "TamatchSaturatePass took " << duration.count() << " µs\n");
   }
 };
 } // namespace
