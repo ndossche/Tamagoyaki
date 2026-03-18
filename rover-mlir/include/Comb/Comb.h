40899e9fac6a24228c026d20f31f7ab0f92bc7ba
jumerckx <31353884+jumerckx@users.noreply.github.com>
2026-03-18 17:24:28 +0100

    remove gmp.h and mpfr.h and fix Comb.h (#22)
    * remove gmp.h and mpfr.h and fix Comb.h
    
    * explicit dependency on mpfr
    
    * debug CI
    
    * fix?

--- rover-mlir/include/Comb/Comb.h
+++ rover-mlir/include/Comb/Comb.h
@@ -1,12 +1,13 @@
 #pragma once
 
-#include "CombDialect.h.inc"
 #include "mlir/Dialect/Arith/IR/Arith.h"
 #include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/IR/BuiltinTypes.h"
 #include "mlir/IR/Dialect.h"
 #include "mlir/IR/OpDefinition.h"
 #include "mlir/Interfaces/SideEffectInterfaces.h"
+
+#include "CombDialect.h.inc"
 
 // #include "CombMLIREnums.h.inc"
 
