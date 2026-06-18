30f1b4ef66575800c556b74abb73ad2f4e9020ef
jumerckx <31353884+jumerckx@users.noreply.github.com>
2026-06-18 14:22:39 +0100

    add speculatability traits to equivalence.graph and equivalence.yield (#74)

--- include/EquivalenceDialect.h
+++ include/EquivalenceDialect.h
@@ -17,6 +17,7 @@
 #include "mlir/IR/OpDefinition.h"
 #include "mlir/IR/PatternMatch.h"
 #include "mlir/IR/RegionKindInterface.h"
+#include "mlir/Interfaces/ControlFlowInterfaces.h"
 #include "mlir/Interfaces/InferTypeOpInterface.h"
 #include "mlir/Interfaces/SideEffectInterfaces.h"
 #include "mlir/Pass/Pass.h"
